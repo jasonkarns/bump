873359000b2b43f2aba1508da281778d5ac39442
Jason Karns <jason@karns.name>
2016-07-30 20:50:41 -0400

    Enable focus filtering

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,4 +38,6 @@ end
 RSpec.configure do |c|
   c.include SpecHelpers::InstanceMethods
   c.extend SpecHelpers::ClassMethods
+  c.filter_run_including :focus => true
+  c.run_all_when_everything_filtered = true
 end
